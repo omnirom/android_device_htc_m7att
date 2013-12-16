9396266e022ab103d9cd240ed415d3b87311b5a5
David Gasaway <ponsasinorem@gmail.com>
2013-12-15 20:03:48 -1000

    m7att: remove call to non-existent file at device/htc/m7ul/custom_m7att.mk
    Change-Id: Ibeb0b0da990e06ec614013d9daf249d809e7a00e

--- aosp_m7att.mk
+++ aosp_m7att.mk
@@ -13,6 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, device/htc/m7ul/custom_m7att.mk)
-
 PRODUCT_NAME := aosp_m7att
