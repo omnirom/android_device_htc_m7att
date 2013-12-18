39659eb6ff5909a009c62239abf7e5ffb38e4ef3
David Gasaway <ponsasinorem@gmail.com>
2013-12-17 15:20:57 -1000

    m7att: remove call to nonexistent overlay
    Change-Id: Idf8372783bb30aa292e62613891e5910fc1ee636

--- device.mk
+++ device.mk
@@ -17,9 +17,6 @@
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/htc/m7att/m7att-vendor.mk)
 
-# overlays
-DEVICE_PACKAGE_OVERLAYS += device/htc/m7att/overlay
-
 # common overlays
 DEVICE_PACKAGE_OVERLAYS += device/htc/m7-common/overlay-gsm
 
